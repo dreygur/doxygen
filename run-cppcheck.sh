182fed97b26fbfafcbaff7d9658a7286bb7851f9
Rakibul Yeasin <rytotul@gmail.com>
2021-07-25 12:51:48 +0600

    Generate Report

--- run-cppcheck.sh
+++ run-cppcheck.sh
@@ -24,14 +24,21 @@ cppcheck "$INPUT_PATH" \
     ${ENABLE_INCONCLUSIVE:+--inconclusive} \
     ${VERBOSE:+--verbose} \
     ${CHECK_CONFIG:+--check-config} \
-    ${GENERATE_REPORT:+--output-file=$REPORT_FILE} \
-    -j "$(nproc)" \
-    --xml \
-    "$INPUT_INCLUDE_DIRECTORIES" \
-    "$INPUT_EXCLUDE_FROM_CHECK" \
     --error-exitcode="$INPUT_ERROR_EXIT_CODE"
 
 if [ "$GENERATE_REPORT" ]; then
+    cppcheck "$INPUT_PATH" \
+        --enable="$INPUT_ENABLED_CHECKS" \
+        ${ENABLE_INCONCLUSIVE:+--inconclusive} \
+        ${VERBOSE:+--verbose} \
+        ${CHECK_CONFIG:+--check-config} \
+        ${GENERATE_REPORT:+--output-file=$REPORT_FILE} \
+        -j "$(nproc)" \
+        --xml \
+        "$INPUT_INCLUDE_DIRECTORIES" \
+        "$INPUT_EXCLUDE_FROM_CHECK" \
+        --error-exitcode="$INPUT_ERROR_EXIT_CODE"
+
     cppcheck-htmlreport \
         --file="$REPORT_FILE" \
         --title="$INPUT_REPORT_NAME" \
