b2cea8d9fd359adb4f2af2ea163ea1415e1033a1
Rakibul Yeasin <rytotul@gmail.com>
2021-07-25 13:02:16 +0600

    Generate Report

--- run-cppcheck.sh
+++ run-cppcheck.sh
@@ -10,11 +10,6 @@ if [ "$INPUT_VERBOSE" = 'true' ]; then
     VERBOSE='yep'
 fi
 
-if [ "$INPUT_GENERATE_REPORT" = 'true' ]; then
-    GENERATE_REPORT='yep'
-    REPORT_FILE=report.xml
-fi
-
 if [ "$INPUT_ENABLED_INCONCLUSIVE" = 'true' ]; then
     ENABLE_INCONCLUSIVE='yep'
 fi
@@ -25,22 +20,3 @@ cppcheck "$INPUT_PATH" \
     ${VERBOSE:+--verbose} \
     ${CHECK_CONFIG:+--check-config} \
     --error-exitcode="$INPUT_ERROR_EXIT_CODE"
-
-if [ "$GENERATE_REPORT" ]; then
-    cppcheck "$INPUT_PATH" \
-        --enable="$INPUT_ENABLED_CHECKS" \
-        ${ENABLE_INCONCLUSIVE:+--inconclusive} \
-        ${VERBOSE:+--verbose} \
-        ${CHECK_CONFIG:+--check-config} \
-        ${GENERATE_REPORT:+--output-file=$REPORT_FILE} \
-        -j "$(nproc)" \
-        --xml \
-        "$INPUT_INCLUDE_DIRECTORIES" \
-        "$INPUT_EXCLUDE_FROM_CHECK" \
-        --error-exitcode="$INPUT_ERROR_EXIT_CODE"
-
-    cppcheck-htmlreport \
-        --file="$REPORT_FILE" \
-        --title="$INPUT_REPORT_NAME" \
-        --report-dir=output
-fi
